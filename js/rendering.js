c24e426015d71f730853682d43cab81057ed6469
Ирина <taffyself@gmail.com>
2020-02-09 22:29:29 +0300

    Учебный проект: pimp my mage
    Добавлен модуль js/backend.js для Ajax запросов
    Добавлен шаблон для отображения статуса отправки данных в форме настройки персонажа
    В модуле js/rendering.js данные для отображения персонажей теперь берутся с сервера
    В модуле js/settings.js данные из формы отправляются через Ajax запрос на сервер

--- js/rendering.js
+++ js/rendering.js
@@ -1,15 +1,19 @@
 'use strict';
 
 (function () {
+  var PERSONAGES_QUANTITY = 4;
+
+  var similarPersonageMessageError = 'Не удалось загрузить похожих персонажей.';
   var similarPersonageTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var similarListElementSelector = '.setup-similar-list';
+  var setupFooter = document.querySelector('.setup-footer');
 
   var createPersonageElement = function (personage) {
     var element = similarPersonageTemplate.cloneNode(true);
 
     element.querySelector('.setup-similar-label').textContent = personage.name;
-    element.querySelector('.wizard-coat').style.fill = personage.coatColor;
-    element.querySelector('.wizard-eyes').style.fill = personage.eyesColor;
+    element.querySelector('.wizard-coat').style.fill = personage.colorCoat;
+    element.querySelector('.wizard-eyes').style.fill = personage.colorEyes;
 
     return element;
   };
@@ -23,7 +27,7 @@
     var fragment = document.createDocumentFragment();
     var element = document.querySelector(selector);
 
-    for (var i = 0; i < personages.length; i++) {
+    for (var i = 0; i < PERSONAGES_QUANTITY; i++) {
       fragment.appendChild(createPersonageElement(personages[i]));
     }
 
@@ -34,6 +38,17 @@
     }
   };
 
-  renderPersonages(window.generation.personages, similarListElementSelector);
+  var onSuccess = function (personages) {
+    renderPersonages(personages, similarListElementSelector);
+  };
+
+  var onError = function (error) {
+    var errorMessage = document.createElement('div');
+    errorMessage.style = 'margin: 15px 0; font-size: 12px; color: rgba(255,255,255,.8); padding-left: 20px;';
+    errorMessage.innerText = similarPersonageMessageError + ' ' + error;
+    setupFooter.insertBefore(errorMessage, setupFooter.firstChild);
+  };
+
+  window.backend.load(onSuccess, onError);
 
 })();
